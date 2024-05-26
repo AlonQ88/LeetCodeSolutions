632577f2fa712744bf262ea1ed53cb5bb6cbe609
Alonso Quesada <alonq@hotmail.com>
2024-05-26 11:03:15 -0600

    Added solution to Roman to Integer mapping problem

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LeetCodeSolutions;
+
+Console.WriteLine(RomanToInteger.RomanToInt("III")); //3
+
+Console.WriteLine(RomanToInteger.RomanToInt("LVIII")); //58
+
+Console.WriteLine(RomanToInteger.RomanToInt("MCMXCIV")); //1994
+
+
+Console.ReadLine();
