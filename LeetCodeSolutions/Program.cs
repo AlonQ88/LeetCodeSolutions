394e00277c9806a9ffe9943d75de705a67808463
Alonso Quesada <alonq@hotmail.com>
2024-05-26 11:20:34 -0600

    Added PalindromeNumber problem solution

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -1,10 +1,15 @@
 ﻿using LeetCodeSolutions;
 
+//Roman to Integer Tests
 Console.WriteLine(RomanToInteger.RomanToInt("III")); //3
 
 Console.WriteLine(RomanToInteger.RomanToInt("LVIII")); //58
 
 Console.WriteLine(RomanToInteger.RomanToInt("MCMXCIV")); //1994
+
+//Palindrome Tests
+
+Console.WriteLine(PalindromeNumber.IsPalindrome(121)); //true
 
 
 Console.ReadLine();
